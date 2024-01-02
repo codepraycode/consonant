516bccee93d161ecc8c7e9d462e72590336aadea
Precious Olusola <preciousolusola16@gmail.com>
2024-01-02 22:31:22 +0100

    Added dropdown for authentication

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,14 +1,55 @@
+'use client'
 import Link from "next/link";
-import SearchFiles from "./Search"
+import { useRouter } from "next/navigation";
+import { useState } from "react";
 
 
 const Header = () => {
+
+    const [showMenu, setShowMenu] = useState(false);
+    const navigation = useRouter();
+
     return (
         <header>
 
-            <div className="nav">
-                <Link href={"/admin"}>codepraycode</Link>
-            </div>
+            <nav className="nav" role="navigation">
+                {/* <h1>Consonant</h1> */}
+                <ul className="nav-list" role="list">
+                    <li>
+                        <Link
+                            href={"#"}
+                            className="nav-list-dropdown-anchor"
+                            data-open={showMenu}
+
+                            onClick={(e)=>{
+                                e.preventDefault();
+
+                                setShowMenu(true)
+                            }}
+
+                            onBlur={()=>setShowMenu(false)}
+                        >
+                            codepraycode
+                        </Link>
+                        <ul className="nav-list-dropdown" role="list">
+                            <li>
+                                <Link href={"/admin"}>Your dashboard</Link>
+                            </li>
+
+                            <li>
+                                <Link
+                                    href={"/logout"}
+                                    onClick={(e)=>{
+                                        e.preventDefault();
+                                        // window.open('/login', null)
+                                        navigation.replace('/login')
+                                    }}
+                                >Logout</Link>
+                            </li>
+                        </ul>
+                    </li>
+                </ul>
+            </nav>
             
         </header>
     )

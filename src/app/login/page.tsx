60d6abf3989bfa9e5b0ce13be9fc9e3e4ba8388d
Precious Olusola <preciousolusola16@gmail.com>
2024-01-02 23:04:09 +0100

    updated login styles

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,5 +1,6 @@
 'use client'
 import { TextInput } from "@/components/Form";
+import Link from "next/link";
 
 
 
@@ -13,15 +14,23 @@ const LoginPage = () => {
     return (
         <section className="form-section">
 
+
+            <h1 className="fs-3 fw-800 text-center">
+                Consonant
+            </h1>
+            <br/>
+            {/* <br/> */}
+
             <div className="form-card">
 
-                <h1>
+                <h2 className="form-card-head">
                     <span>Sign into your account</span>
 
+                    <br/>
 
-                    {/* <span>Don&#39;t have an account</span> */}
+                    {/* <Link href={"/"}>Don&#39;t have an account</Link> */}
 
-                </h1>
+                </h2>
 
                 <form onSubmit={handleLogin} className="mt-1">
                     <TextInput

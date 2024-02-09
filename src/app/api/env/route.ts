4bae3fdb05c1cbedbc9b6a179f5ac62d8c70f601
Precious Olusola <preciousolusola16@gmail.com>
2024-02-09 09:16:44 +0100

    Updated new endpoint

--- src/app/api/env/route.ts
+++ src/app/api/env/route.ts
@@ -5,10 +5,11 @@ export function GET() {
 
 
     // ! Verify header, as there is a bug here
-
     return NextResponse.json({
-        SUPERBASE_API_KEY: env['SUPERBASE_API_KEY'],
-        SUPERBASE_URL: env['SUPERBASE_URL'],
+        supabase: {
+            SUPERBASE_API_KEY: env['SUPERBASE_API_KEY'],
+            SUPERBASE_URL: env['SUPERBASE_URL'],
+        }
     })
     
 }

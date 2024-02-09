1676f2784b3018fe85efc9fbbda27e61be2e2852
Precious Olusola <preciousolusola16@gmail.com>
2024-02-09 08:51:48 +0100

    Added endpoint for cred

--- src/app/api/env/route.ts
+++ src/app/api/env/route.ts
@@ -3,6 +3,9 @@ import { NextResponse } from "next/server"
 export function GET() {
     const env = process.env;
 
+
+    // ! Verify header, as there is a bug here
+
     return NextResponse.json({
         SUPERBASE_API_KEY: env['SUPERBASE_API_KEY'],
         SUPERBASE_URL: env['SUPERBASE_URL'],
